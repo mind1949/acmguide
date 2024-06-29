5ad30ad66f385430ab0293acba18b621e40c1d02
mind1949 <lianjie1949@gmail.com>
2024-06-29 18:16:59 +0800

    complete ch0102

---
+++ ch0102_a*b%p.go
@@ -0,0 +1,26 @@
+package acmguide
+
+// Mul
+//
+// 例题: ch0102/acmwing90 https://ac.nowcoder.com/acm/contest/996/C
+//
+//	求 a 乘 b 对 p 取模的值，其中 1≤a,b,p≤10181≤a,b,p≤1018。
+//
+// (a*b)%p = ((a%p)*(b%p)))%p
+// (a+b)%p = ((a%p)+(b%p))%p
+func Mul(a, b, p int) int {
+	result := 1 / p
+	a %= p
+	b %= p
+	for b > 0 {
+		if b&1 == 1 {
+			// 再次用到 (a+b)%p = ((a%p)+(b%p))%p
+			// 防止整数溢出
+			result = (result + a) % p
+		}
+		a = a * 2 % p
+		b >>= 1
+	}
+
+	return result % p
+}
